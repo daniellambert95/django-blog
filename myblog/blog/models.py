f714c66df7d5cc7743887e0b2b899d97a6c735ab
temporaryadmin <danjlambert95@gmail.com>
2021-10-20 10:03:37 +0200

    made some edits to the  view, created our own templates folder

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -1,16 +1,17 @@
-# A model is the single, definitive source of information about your data. 
-# It contains the essential fields and behaviors of the data you’re storing. 
+# A model is the single, definitive source of information about your data.
+# It contains the essential fields and behaviors of the data you’re storing.
 # Generally,each model maps to a single database table.
 
 # Each attribute of the model represents a database field.
 
 from django.db import models
 
+
 class Blog(models.Model):
     title = models.CharField(max_length=150)
     body = models.TextField()
     date = models.DateTimeField(auto_now=True)
 
-# function __str__ is defining the title of the blog (returns title of the class)
+    # function __str__ is defining the title of the blog (returns title of the class)
     def __str__(self):
         return self.title
