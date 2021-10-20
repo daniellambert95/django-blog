19aa82c4355afac112bdce385e66c51f696e7b18
temporaryadmin <danjlambert95@gmail.com>
2021-10-20 11:39:21 +0200

    updates to views.py

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -10,16 +10,8 @@ def home(request):
     blog_posts = Blog.objects.all()
     context = {"blog_posts": blog_posts}
     return render(request, "blog/home.html", context)
-    # blog_list_html = ""
-    # for blog in blog_posts:
-    #     blog_list_html += f"<p><a href='/blog/{blog.id}'>{blog.title}</a></p>"
-    # html = f"<html><body> {blog_list_html}</body></html>"
-    # return HttpResponse(html)
 
-
-def blog_post(request, id=1):
+def blog_post(request, id):
     blog = Blog.objects.get(id=id)
     context = {"blog": blog}
     return render(request, "blog/blog_post.html", context)
-    # html = f"<html><body><h1>{blog_post.title}</h1>{blog_post.date}<p>{blog_post.body}</p></body></html>"
-    # return HttpResponse(html)
